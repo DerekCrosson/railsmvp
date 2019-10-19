16774305099c0f202c97fd5586a178fcdf416184
Derek Crosson <derek@designcafe.co.za>
2019-10-19 17:43:09 +0200

    Fixed NameError: uninitialized constant Application

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,10 +1,10 @@
-unless Application.config.action_mailer.nil?
+unless Rail.config.action_mailer.nil?
 
-  Application.config.action_mailer.smtp_settings = {}
+  Rails.config.action_mailer.smtp_settings = {}
 
   options = YAML.load_file(Rails.root.join('config', 'smtp.yml'))[Rails.env]
 
   options.each do |name, value|
-    Application.config.action_mailer.smtp_settings[name.to_sym] = value
+    Rails.config.action_mailer.smtp_settings[name.to_sym] = value
   end unless options.nil?
 end
