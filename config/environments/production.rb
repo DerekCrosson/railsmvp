4832ed6743231fb0e38dd3b8656fcabdd64abb57
Derek Crosson <derek@designcafe.co.za>
2019-10-19 15:14:56 +0200

    Remove duplicated line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,9 +32,6 @@ Rails.application.configure do
   config.assets.compile = true #set to false by default
   config.assets.digest = true
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
