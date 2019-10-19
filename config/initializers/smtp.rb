e250ab4604959f3ace99086d3a1a4bb68b18459b
Derek Crosson <derek@designcafe.co.za>
2019-10-19 18:02:52 +0200

    Try a different way of loading smtp settings

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,7 +2,7 @@ unless Rails.application.config.action_mailer.nil?
 
   Rails.application.config.action_mailer.smtp_settings = {}
 
-  options = YAML.load_file(Rails.root.join('config', 'smtp.yml'))[Rails.env]
+  options = YAML.load_file("#{Rails.root}/config/smtp.yml")
 
   options.each do |name, value|
     Rails.application.config.action_mailer.smtp_settings[name.to_sym] = value
