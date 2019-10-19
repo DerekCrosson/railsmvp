59e5c62dbce77c56a66c5ddd9506d62182f44f5c
Derek Crosson <derek@designcafe.co.za>
2019-10-19 17:47:24 +0200

    Making mistakes because I am sleepy...

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,10 +1,10 @@
-unless Rails.config.action_mailer.nil?
+unless Rails.application.config.action_mailer.nil?
 
-  Rails.config.action_mailer.smtp_settings = {}
+  Rails.application.config.action_mailer.smtp_settings = {}
 
   options = YAML.load_file(Rails.root.join('config', 'smtp.yml'))[Rails.env]
 
   options.each do |name, value|
-    Rails.config.action_mailer.smtp_settings[name.to_sym] = value
+    Rails.application.config.action_mailer.smtp_settings[name.to_sym] = value
   end unless options.nil?
 end
